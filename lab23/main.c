183fb7522cca27f01ecb5139c9bda5156481460e
keinpop <artem-sasha04@mail.ru>
2023-03-22 18:54:30 +0300

    Added full redacted lab23

--- lab23/main.c
+++ lab23/main.c
@@ -4,18 +4,17 @@
 #include <stdbool.h>
 #include "tree.h"
 
-
 int main(void)
 {
     char s[8];
 
-    Tree tree = NULL;
-    int root = 0, ver = 0, parent = 0;
+    Tree tree = NULL; // Указатель на корень дерева
+    int root = 0, ver = 0, parent = 0; // Переменные для новых узлов дерева
 
-    printf("\nPrint 'help' for more information\n\n");
+    printf("\nPrint 'help' for more information\n\n"); // Выводим подсказку при запуске программы
     while (1) {
-        scanf("%7s", s);
-        if (!strcmp(s, "insert") || !strcmp(s, "ins")) {
+        scanf("%7s", s); // Ввод символа с клавиатуры
+        if (!strcmp(s, "insert") || !strcmp(s, "ins")) { // Задаем корень дерева
             if(!tree) {
                 printf("Add the value of the tree root:\n");
                 scanf("%d", &root);
@@ -24,34 +23,34 @@ int main(void)
             while (scanf("%d%d", &parent, &ver)) {
                 tree_add_node(tree, parent, ver);
             }
-        } else if (!strcmp(s, "delete")) {
-            if(!tree) printf("Tree doesnt exist, use command 'help'\n");
+        } else if (!strcmp(s, "delete")) { // Если команда delete
+            if(!tree) printf("Tree doesnt exist, use command 'help'\n"); //Если нет дерева, выводим подсказку
             else {
                 scanf("%d", &ver);
-                tree_del_node(tree, ver);
+                tree_del_node(tree, ver); // Удаляем зачение
             }
-        } else if (!strcmp(s, "quit")) {
-            if (tree) tree_destroy(tree);
+        } else if (!strcmp(s, "quit")) { // Если команда quit
+            if (tree) tree_destroy(tree); // Рушим дерево 
             break;
-        } else if (!strcmp(s, "run")) {
-            if(!tree) printf("Tree doesnt exist, use command 'help'\n");
+        } else if (!strcmp(s, "run")) { // Если команда run, начинаем проверку на монотонность
+            if(!tree) printf("Tree doesnt exist, use command 'help'\n"); 
             else {
                 check_monotonicity_of_decreaset(tree);
             }
-        } else if (!strcmp(s, "print")) {
+        } else if (!strcmp(s, "print")) { // Если команда print, выводим дерево
             if (!tree) printf("Tree doesnt exist, use command 'help'\n");
             else {
                 printf("\n\n");
                 tree_print(tree);
                 printf("\n\n");
             }
-        } else if (!strcmp(s, "destroy")) {
+        } else if (!strcmp(s, "destroy")) { // Удаляем дерево
             if (!tree) printf("Tree doesnt exist, use command 'help'\n");
             else {
                 tree_destroy(tree);
                 tree = NULL;
             }
-        } else if (!strcmp(s, "help")) {
+        } else if (!strcmp(s, "help")) { // Помощник
             printf("\n\nCommand 'insert' - if the tree doesnt exist, makes a tree. Command 'ins' - if tree was made, adds vertices in the tree\n\n");
             printf("Command 'delete num' deletes all the vertices and all her children\n\n");
             printf("Command 'print' prints the vertices of the tree\n\n");
