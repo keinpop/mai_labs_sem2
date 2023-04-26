391012d581f437cfe3d0f90d81eee6c72b3bfdfd
keinpop <artem-sasha04@mail.ru>
2023-04-26 18:35:53 +0300

    Added first develop kp6

---
+++ kp6/computer.h
@@ -0,0 +1,23 @@
+#ifndef _COMPUTER_H_
+#define _COMPUTER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _comp 
+{
+    char surname[50]; // Фамилия студента
+    int num_of_proc; // Значение процессора
+    char type_of_proc[50]; // Тип процессора
+    int mem_size_ram; // Размер оперативной памяти
+    char type_of_contr[50]; // Тип плажки оперативы
+    int mem_size_video_proc; // Объем видеопамяти
+    char type_of_disk[50]; // Тип хранилища
+    int num_of_disc; // Количество хранилищ
+    int cap_of_disc; // Объем хранилища в Гб
+    int num_of_intcontr; // Количество интерфейсных контроллеров
+    int num_of_dev; // Количество устройств i/o
+    char os[50];
+} pc;
+
+#endif //_COMPUTER_H_
